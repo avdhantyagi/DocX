24b36d543b4a79f368197b5872ada379a4956344
avdhantyagi <avdhantyagi1011@gmail.com>
2024-09-13 20:25:45 +0530

    DocumentId connection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import TextEditor from './TextEditor'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import { v4 as uuidv4} from 'uuid'
 
 function App() {
-
   return (
-    <>
-    <TextEditor />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidv4()}`} replace />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
