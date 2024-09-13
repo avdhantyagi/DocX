38e69ba7a5566dccc30f225092ad28685a2c4016
avdhantyagi <avdhantyagi1011@gmail.com>
2024-09-13 19:54:17 +0530

    Localhost synced

--- src/TextEditor.jsx
+++ src/TextEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
 import { io } from 'socket.io-client'
@@ -16,13 +16,47 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const [socket, setSocket] = useState()
+    const [quill, setQuill] = useState()
+
+
     useEffect(() => {
-        const socket = io("http://localhost:5174")
+        const s = io("http://localhost:5174")
+        setSocket(s)
         
         return () => {
-            socket.disconnect()
+            s.disconnect()
         }
     }, [])
+
+    useEffect(() => {
+        if(socket ==null || quill == null) return
+
+        const handler = (delta, oldDelta, source) => {
+            if(source !== 'user') return
+            socket.emit("send-changes", delta)
+        }
+
+        quill.on('text-change', handler)
+        
+        return() =>{
+            quill.off('text-change', handler)
+        }
+    },[socket,quill])
+
+    useEffect(() => {
+        if(socket ==null || quill == null) return
+
+        const handler = (delta) => {
+            quill.updateContents(delta)
+        }
+
+        socket.on('receive-changes', handler)
+        
+        return() =>{
+            socket.off('receive-changes', handler)
+        }
+    },[socket,quill])
 
     const wrapperRef = useCallback (wrapper => {
         if (wrapper == null) return
@@ -30,7 +64,8 @@ export default function TextEditor() {
         wrapper.innerHTML = ""
         const editor = document.createElement('div')
         wrapper.append(editor)
-        new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} });
+        const q = new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} });
+        setQuill(q)
     },[])
     return <div className="container" ref={wrapperRef}></div>
 }
